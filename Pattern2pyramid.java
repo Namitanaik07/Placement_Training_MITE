3bafdac3994d293d5ac315b9e210a31f6bed7b61
Namita Naik <155013110+Namitanaik07@users.noreply.github.com>
2026-01-21 11:13:34 +0530

    Add Pattern2pyramid class to print pyramid pattern

---
+++ Pattern2pyramid.java
@@ -0,0 +1,15 @@
+public class Pattern2pyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=(n-i);k++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        
+    }
+}
