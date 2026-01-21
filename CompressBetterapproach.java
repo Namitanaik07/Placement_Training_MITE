1fa68200b548a8fe7118b75525a59d490dc4f205
Namita Naik <155013110+Namitanaik07@users.noreply.github.com>
2026-01-21 16:22:44 +0530

    Create CompressBetterapproach.java

---
+++ CompressBetterapproach.java
@@ -0,0 +1,21 @@
+public class CompressBetterBetter {
+    public static String compressBetter(int[] arr) {
+    if (arr == null || arr.length == 0) return "";
+    
+    StringBuilder result = new StringBuilder();
+    int count = 1;
+
+    for (int i = 1; i <= arr.length; i++) {
+        
+        if (i == arr.length || arr[i] != arr[i - 1]) {
+            result.append("(").append(arr[i - 1]).append(",").append(count).append(") ");
+            count = 1; 
+        } else {
+            count++;
+        }
+    }
+    return result.toString().trim();
+}
+
+
+}
