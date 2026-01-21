9f33ef723df237d2312b4b44f685e9fbbee4df5c
Namita Naik <155013110+Namitanaik07@users.noreply.github.com>
2026-01-21 11:12:18 +0530

    Add Pattern1 class to print a pattern of stars and underscores

---
+++ Patter1.java
@@ -0,0 +1,13 @@
+public class Pattern1 {
+    public static void main(String args[]){
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(j%2==0){
+                    System.out.print("*");
+                }else{System.out.print("_");}
+            }
+            System.out.println("");
+        }
+    }
+}
